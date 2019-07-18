2149068c6dec19edc33010bf2026a41dd3503c2f
Asbjørn Sloth Tønnesen <ast@2e8.dk>
2019-07-18 17:49:05 +0000

    define inet.mixed_networks
    Signed-off-by: Asbjørn Sloth Tønnesen <ast@2e8.dk>

--- lua/inet/init.lua
+++ lua/inet/init.lua
@@ -10,6 +10,12 @@ function mt.__call(_, ...)
 	return new_inet(...)
 end
 
+do
+	local mixed_networks = set.new()
+	mixed_networks:add(new_inet('::ffff:0.0.0.0/96'))
+	M.mixed_networks = mixed_networks
+end
+
 M.is4 = core.is_inet4
 M.is6 = core.is_inet6
 M.is  = core.is_inet
